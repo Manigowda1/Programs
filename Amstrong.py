f9e4a20f59ffa07d6f90c045d60da7c38e4eec09
Manikanta Gowda B <78212802+Manigowda1@users.noreply.github.com>
2022-04-18 17:25:43 +0530

    Update Amstrong.py
    Adding method 2

--- Amstrong.py
+++ Amstrong.py
@@ -16,6 +16,26 @@ if(s==n):
     print("It is")
 else:
     print("It is not")
+    
+    
+ '''Method 2 for order 3'''
+
+n=int(input("Enter a number to check "))
+t1=n
+sum=0
+while n!=0:
+    t=n%10
+    sum=sum+t**3
+    n=n//10
+
+if sum==t1:
+    print("It is Amrstrong number")
+else:
+    print("Not an armstrong number")
+
+
+
+
 
 
 
